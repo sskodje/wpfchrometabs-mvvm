00bc9534d0cc1743c5b66dab6d7951d4bca69a4c
Sverre <sskodje@gmail.com>
2017-08-10 19:17:32 +0200

    Fixed a few bug in the custom tab shape demo project.

--- Demo/CustomTabShape.cs
+++ Demo/CustomTabShape.cs
@@ -41,7 +41,7 @@ namespace Demo
 
         private Geometry GetGeometry()
         {
-            double width = this.ActualWidth - StrokeThickness;
+            double width = this.DesiredSize.Width - StrokeThickness;
             double height = 25;
             double x1 = width - 15;
             double x2 = width - 10;
@@ -51,7 +51,7 @@ namespace Demo
             //For some reason this is needed to update the visual if the initial width is 0
             if (this.ActualWidth > 0 && _lastRenderedWidth == 0)
                 InvalidateVisual();
-            _lastRenderedWidth = this.ActualWidth;
+            _lastRenderedWidth = this.DesiredSize.Width;
 
             return Geometry.Parse(String.Format(CultureInfo.InvariantCulture, "M0,{5} C0,0 2,0 10,0 15,0 {0},0 {1},0 {2},0 {3},0 {4},{5}", x1, x2, x3, x4, x5, height));
         }
